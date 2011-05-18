Aj0Ay/fedfs-utils

commit d2ff4e04189281736a960014495d65b78ea6dbbb

nsdb-nces: Minor clean ups
The usage message lists a "-e" option that does not appear to be
supported.

Also, make the output formatting a little neater.  Report the name of
the NSDB on the output in case no hostname was specified on the
command line.

Finally, nsdb-nces was always completing with EXIT_FAILURE.  When an
NCE is detected, complete with EXIT_SUCCESS.

Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

src/nsdbc/nsdb-nces.c
@@ -71,7 +71,7 @@ nsdb_nces_usage(const char *progname)
 {
 	fprintf(stderr, "\n%s version " VERSION "\n", progname);
 	fprintf(stderr, "Usage: %s [ -d ] "
-			"[ -l nsdbname ] [ -r nsdbport ] [ -e nce ]\n\n",
+			"[ -l nsdbname ] [ -r nsdbport ]\n\n",
 			progname);
 
 	fprintf(stderr, "\t-?, --help           Print this help\n");
@@ -211,18 +211,19 @@ main(int argc, char **argv)
 		goto out_close;
 	}
 
+	printf("NSDB: %s:%u\n", nsdbname, nsdbport);
 	for (i = 0; contexts[i] != NULL; i++) {
 		char *dn;
 
-		printf("namingContext: %s\n", contexts[i]);
 		retval = nsdb_get_nceprefix_s(host, contexts[i], &dn, &ldap_err);
+		printf("  namingContext '%s' ", contexts[i]);
 		if (retval == FEDFS_OK) {
-			printf("The namingContext '%s' is a FedFS NCE.  "
-				"The top-level entry of this FedFS DIT "
-				"is '%s'.\n\n", contexts[i], dn);
+			printf("is a FedFS NCE, DIT starts at '%s'.\n",
+				contexts[i], dn);
 			free(dn);
+			exit_status = EXIT_SUCCESS;
 		} else
-			printf("\n");
+			printf("is not a FedFS NCE.\n");
 	}
 
 	nsdb_free_string_array(contexts);

commit 35ae78cedafc125175d10a159fa6193b1eae7905

nsdb-nces: Fix incorrect output
Commit d2ff4e04 introduced changes to show the NCE prefix of each NCE
on an NSDB.  The command shows the naming context DN instead of the
NCE prefix DN.  I didn't catch this because my test NSDB has a single
NCE where the naming context DN is the same as the NCE DN.

Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

src/nsdbc/nsdb-nces.c
@@ -218,8 +218,7 @@ main(int argc, char **argv)
 		retval = nsdb_get_nceprefix_s(host, contexts[i], &dn, &ldap_err);
 		printf("  namingContext '%s' ", contexts[i]);
 		if (retval == FEDFS_OK) {
-			printf("is a FedFS NCE, DIT starts at '%s'.\n",
-				contexts[i], dn);
+			printf("is a FedFS NCE, DIT starts at '%s'.\n", dn);
 			free(dn);
 			exit_status = EXIT_SUCCESS;
 		} else