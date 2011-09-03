cfab6c8fc4589116f24e16dcf895937481bade40
Chuck Lever <chuck.lever@oracle.com>
2011-09-03 12:11:30 -0400

    libnsdb: Add method to split NCE DN
    Before NSDB container info can be set up, the NCE DN has to be
    split into the namingContext piece and the NCE prefix piece.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- src/include/nsdb.h
+++ src/include/nsdb.h
@@ -313,6 +313,9 @@ FedFsStatus	 nsdb_get_nceprefix_s(nsdb_t host, const char *naming_context,
 				char **dn, unsigned int *ldap_err);
 FedFsStatus	 nsdb_get_naming_contexts_s(nsdb_t host, char ***contexts,
 				unsigned int *ldap_err);
+FedFsStatus	 nsdb_split_nce_dn_s(nsdb_t host, const char *nce,
+				char **context, char **prefix,
+				unsigned int *ldap_err);
 
 /**
  * Resolve an FSN (5.2.2)
